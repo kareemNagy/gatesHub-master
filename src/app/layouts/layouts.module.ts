77e969472f7678d73602dba0aa2918f28ef82d14
Kareem Nagy <Kareem.Nagy@linkdev.com>
2021-06-25 13:39:29 +0200

    menu desktop - responsive

--- src/app/layouts/layouts.module.ts
+++ src/app/layouts/layouts.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderModule } from './header/header.module';
 import { FooterModule } from './footer/footer.module';
+import { NavigationModule } from './navigation/navigation.module';
 
 @NgModule({
   imports: [
     CommonModule,
     HeaderModule,
-    FooterModule
+    FooterModule,
   ],
   declarations: [],
   exports: [HeaderModule, FooterModule]
---
+++ src/app/layouts/navigation/navigation.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.scss']
+})
+export class NavigationComponent implements OnInit {
+  @Input() visbility: boolean = false;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
