54adbb8fca093ddf4f4e5d7d4d0523ed7c31eef0
Kareem Nagy <Kareem.Nagy@linkdev.com>
2021-06-28 21:35:56 +0200

    fix menu responsive and desktop with close

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   public visbility: boolean = false
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  openMenu() {
+    this.visbility = !this.visbility;
+    document.querySelector('body').classList.toggle('overflow-hidden-cst');
+
   }
 
 }
