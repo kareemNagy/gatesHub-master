c2b03f7954f8eaf91cc557d7917f0be31446c11b
Kareem Nagy <Kareem.Nagy@linkdev.com>
2021-06-28 20:14:47 +0200

    merge

--- src/app/layouts/header/header.module.ts
+++ src/app/layouts/header/header.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header.component';
+import { NavigationModule } from '../navigation/navigation.module';
 
 
 
@@ -9,8 +10,9 @@ import { HeaderComponent } from './header.component';
     HeaderComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    NavigationModule
   ],
-  exports: [HeaderComponent]
+  exports: [HeaderComponent, NavigationModule]
 })
 export class HeaderModule { }
